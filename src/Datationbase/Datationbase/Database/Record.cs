0123d0f34c96ab8851d1bd2679a45b24e147f514
Severin Gafner <severin.gafner@gmail.com>
2021-07-01 14:11:55 +0200

    Made record abstract

--- src/Datationbase/Datationbase/Database/Record.cs
+++ src/Datationbase/Datationbase/Database/Record.cs
@@ -7,5 +7,20 @@ using System.Threading.Tasks;
 
 namespace Datationbase.Database
 {
-    class Record : Dictionary<string, object> {}
+    /**
+     * This class represents a Record (a row) in the database.
+     * To create an own table extend this class and set the fields in the Constructor. 
+     * 
+     * Example: 
+     * class Employee : Record
+     * {
+     *   public Employee(int id, string name, int salary)
+     *   {
+     *       Add("id", id);
+     *       Add("name", name);
+     *       Add("salary", salary);
+     *   }
+     * }
+     */
+    abstract class Record : Dictionary<string, object> {}
 }
