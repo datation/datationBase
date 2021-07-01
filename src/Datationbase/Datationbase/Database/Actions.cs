7521152972478b45766a19c1f744a256d58e7321
Severin Gafner <severin.gafner@gmail.com>
2021-07-01 14:04:02 +0200

    Use Table instead of HashSet, made Select generic

--- src/Datationbase/Datationbase/Database/Actions.cs
+++ src/Datationbase/Datationbase/Database/Actions.cs
@@ -7,19 +7,35 @@ using System.Threading.Tasks;
 
 namespace Datationbase.Database
 {
-    class Actions
+    static class Actions
     {
         /**
+         * The select operator is used to select records in our table.
          * This function will take a list of Records and return only the Records that meet all given predicates.
-         **/
-        public static List<Record> Select(List<Record> table, List<Predicate<Record>> predicates)
+         */
+        public static Table<T> Select<T>(Table<T> table, List<Predicate<T>> predicates) where T : Record
         {
-            List<Record> filteredList = table;
-            foreach (Predicate<Record> predicate in predicates)
+            foreach (Predicate<T> predicate in predicates)
             {
-                filteredList = filteredList.FindAll(predicate);
+                table.filter(predicate);
             }
-            return filteredList;
+            return table;
+        }
+
+        /**
+         * The project operator allows the user to apply filters to the columns (i.e the removing of unnecessary information).
+         */
+        public static Table<Record> Project(Table<Record> table, List<string> columns)
+        {
+            //foreach (Record record in table)
+            //{
+            //    Record newRecoord = new Record();
+            //    columns.ForEach((string column) => {
+            //        newRecoord.Add(column, record[column]);
+            //    });
+            //    record = newRecoord;
+            //});
+            return table;
         }
 
     }
--- src/Datationbase/Datationbase/Program.cs
+++ src/Datationbase/Datationbase/Program.cs
@@ -12,17 +12,17 @@ namespace Datationbase
     {
         static void Main(string[] args)
         {
-            List<Record> employees = new List<Record>();
+            Table<Employee> employees = new Table<Employee>();
             employees.Add(new Employee(1, "Jon Doe", 60000));       // INSERT INTO EMPLOYEE (ID, NAME, SALARY) VALUES (1, "Jon Doe", 60000);
             employees.Add(new Employee(2, "Max Muster", 40000));   // INSERT INTO EMPLOYEE (ID, NAME, SALARY) VALUES (1, "Jon Doe", 60000);
             employees.Add(new Employee(3, "Marie Muster", 80000));
 
-            Predicate<Record> pred = (Record record) => (int)record["salary"] > 50000;  // WHERE SALARY > 50000
-            Predicate<Record> pred2 = (Record record) => (int)record["salary"] < 70000; // WHERE SALARY < 70000
-            List<Predicate<Record>> adsd = new List<Predicate<Record>>();
+            Predicate<Employee> pred = (Employee record) => (int)record["salary"] > 50000;  // SALARY > 50000
+            Predicate<Employee> pred2 = (Employee record) => (int)record["salary"] < 70000; // SALARY < 70000
+            List<Predicate<Employee>> adsd = new List<Predicate<Employee>>();
             adsd.Add(pred);
             adsd.Add(pred2);
-            List<Record> newEmployees = Actions.Select(employees, adsd); // SELECT * FROM Employee WHERE SALARY > 50000 AND SALARY < 70000
+            Table<Employee> newEmployees = Actions.Select(employees, adsd); // SELECT * FROM Employee WHERE SALARY > 50000 AND SALARY < 70000
             Console.WriteLine("yeah");
 
         }
