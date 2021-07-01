da34805cea7728299dd2083b55e412dbaa45c3e9
Severin Gafner <severin.gafner@gmail.com>
2021-07-01 14:03:34 +0200

    Create table class

---
+++ src/Datationbase/Datationbase/Database/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datationbase.Database
+{
+    class Table<T> : HashSet<T> where T : Record
+    {
+        /**
+         * filters the Table by the given predicate
+         */
+        public void filter(Predicate<T> pred)
+        {
+            RemoveWhere((T record) => pred(record) == false);
+        }
+    }
+}
