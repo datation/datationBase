2606689e87284db82f4b6ac774d1c7e045f998e8
Severin Gafner <severin.gafner@gmail.com>
2021-07-01 10:31:15 +0200

    Created Select action and record

---
+++ src/Datationbase/Datationbase/Database/Actions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datationbase.Database
+{
+    class Actions
+    {
+        /**
+         * This function will take a list of Records and return only the Records that meet all given predicates.
+         **/
+        public static HashSet<Record> Select(HashSet<Record> hashSet, List<Predicate<Record>> predicates)
+        {
+            var clonedSet = new HashSet<Record>(hashSet, hashSet.Comparer);
+            foreach (Record record in clonedSet)
+            {
+                foreach (Predicate<Record> predicate in predicates)
+                {
+                    clonedSet.RemoveWhere(predicate);
+                }
+            }
+            return clonedSet;
+        }
+    }
+}
---
+++ src/Datationbase/Datationbase/Database/Record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datationbase.Database
+{
+    class Record
+    {
+        Dictionary<string, object> data;
+        public Record(Dictionary<string, object> data)
+        {
+            this.data = data; ;
+        }
+    }
+}
