437261751be5cbbcc1922f932f79d65e0cfef6d3
Unknown <severin.gafner@gmail.com>
2021-08-16 21:21:19 +0200

    Log employees in Programm

---
+++ src/Datationbase/Datationbase/Database/WriteToSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datationbase.Database
+{
+    class WriteToSystem
+    {
+        WriteToSystem(string tableName)
+        {
+            // using StreamWriter file = new("tableName.txt");
+        }
+    }
+}
--- src/Datationbase/Datationbase/Program.cs
+++ src/Datationbase/Datationbase/Program.cs
@@ -2,9 +2,6 @@
 using Datationbase.Implementation;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Datationbase
 {
@@ -14,23 +11,38 @@ namespace Datationbase
         {
             Table<Record> employees = new Table<Record>();
             employees.Add(Employee.New(1, "Jon Doe", 60000));       // INSERT INTO EMPLOYEE (ID, NAME, SALARY) VALUES (1, "Jon Doe", 60000);
-            employees.Add(Employee.New(2, "Max Muster", 40000));   // INSERT INTO EMPLOYEE (ID, NAME, SALARY) VALUES (1, "Jon Doe", 60000);
+            employees.Add(Employee.New(2, "Max Muster", 40000));   // INSERT INTO EMPLOYEE (ID, NAME, SALARY) VALUES (1, "Max Muster", 40000);
             employees.Add(Employee.New(3, "Marie Muster", 80000));
-
+            log("Alle Mitarbeiter", employees);
             Predicate<Record> pred = (Record record) => (int)record["salary"] > 50000;  // SALARY > 50000
             Predicate<Record> pred2 = (Record record) => (int)record["salary"] < 70000; // SALARY < 70000
-            List<Predicate<Record>> adsd = new List<Predicate<Record>>();
-            adsd.Add(pred);
-            adsd.Add(pred2);
-            Table<Record> newEmployees = Actions.Select(employees, adsd); // SELECT * FROM Employee WHERE SALARY > 50000 AND SALARY < 70000
-            Console.WriteLine("yeah");
+            List<Predicate<Record>> predicates = new List<Predicate<Record>>();
+            predicates.Add(pred);
+            predicates.Add(pred2);
+            Table<Record> newEmployees = Actions.Select(employees, predicates); // SELECT * FROM Employee WHERE SALARY > 50000 AND SALARY < 70000
+            log("Gefilterte Mitarbeiter", newEmployees);
             List<string> fieldsToDisplay = new List<string> { "id", "name" };
             Table<Record> projectedEmployees = Actions.Project(employees, fieldsToDisplay); // SELECT id, name FROM Employee
+            log("Nur ID und name", projectedEmployees);
             Console.WriteLine("yeah2");
             Dictionary<string, string> keysToReplace = new Dictionary<string, string>() { ["name"] = "full name"};
             Table<Record> renamedEmployees = Actions.RenameColumns(employees, keysToReplace); // SELECT *, name AS "full name" as FROM Employee
-            Console.WriteLine("yeah3");
+            log("Full name statt name", renamedEmployees);
+            Console.ReadLine();
 
+        }
+
+        static void log(string title, Table<Record> employees)
+        {
+            Console.Write("\n" + title + "\n\n");
+            foreach(Record record in employees)
+            {
+                foreach(KeyValuePair<string,object> field in record)
+                {
+                    Console.Write(field.Key + ": " + field.Value + ", ");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
