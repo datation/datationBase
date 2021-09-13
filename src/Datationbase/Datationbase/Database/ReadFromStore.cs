888369c386fac804e6235956beaa06e3108d3df2
Dave <davehae99@gmail.com>
2021-09-13 21:24:35 +0200

    fix unneeded method and vars

--- src/Datationbase/Datationbase/Database/ReadFromStore.cs
+++ src/Datationbase/Datationbase/Database/ReadFromStore.cs
@@ -9,21 +9,16 @@ namespace Datationbase.Database
 {
     class ReadFromStore
     {
-        public static T GetInstance<T>(params object[] args)
-        {
-            return (T)Activator.CreateInstance(typeof(T), args);
-        }
-
         public static Table<T> Read<T>() where T : Record
         {
             string filePath = @Config.path + typeof(T).Name + ".csv";
-            using (var reader = new StreamReader(@filePath))
+            using (StreamReader reader = new StreamReader(@filePath))
             {
                 Table<T> returnValue = new Table<T>();
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
                     List<object> result = new List<object>();
                     foreach (string value in values)
                     {
@@ -44,10 +39,6 @@ namespace Datationbase.Database
 
                     }
                     var obj = (T)Activator.CreateInstance(typeof(T), result.ToArray());
-                    //foreach (object value in values)
-                    //{
-                    //}
-
 
                     returnValue.Add(obj);
                 }
